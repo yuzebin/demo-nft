c0493ab26733fb93b78931e89441bb16225e5a85
yuzebin <yuzebin@gmail.com>
2022-08-25 12:54:13 +0800

    add env_example

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,6 @@ async function main() {
 	const MyNFT = await ethers.getContractFactory("MyNFT")
 
 	// Start deployment, returning a promise that resolves to a contract object
-	// const myNFT = await MyNFT.deploy({ value: hre.ethers.utils.parseEther("0.07") })
 	const myNFT = await MyNFT.deploy()
 	console.log("Contract deployed to address:", myNFT.address)
 }
